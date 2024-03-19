1811803084da13ff9c7d61022411d379e2e510a8
Jishnu-k-b <jishnu.kb17291@gmail.com>
2024-03-19 16:07:27 +0530

    Todo CRUD

--- app/apps.py
+++ app/apps.py
@@ -21,3 +21,44 @@ async def create_todo(todo: Todo):
     todo_dict["_id"] = str(inserted_id)
 
     return todo_dict
+
+
+# Read all todo
+@app.get("/todos/")
+async def read_all_todos():
+    todos = []
+    for todo in collection.find():
+        todo["_id"] = str(todo["_id"])
+        todos.append(todo)
+    return todos
+
+
+# Read a todo
+@app.get("/todos/{todo_id}")
+async def read_todo(todo_id: str):
+    todo = collection.find_one({"_id": ObjectId(todo_id)})
+    if todo:
+        todo["_id"] = str(todo["_id"])
+        return todo
+    raise HTTPException(status_code=404, detail="Todo not found")
+
+
+# Update a todo
+@app.put("/todos/{todo_id}")
+async def update_todo(todo_id: str, todo: Todo):
+    todo_dict = todo.model_dump()
+    updated_todo = collection.update_one(
+        {"_id": ObjectId(todo_id)}, {"$set": todo_dict}
+    )
+    if updated_todo.modified_count:
+        return {"message": "Todo updated successfully"}
+    raise HTTPException(status_code=404, detail="Todo not found")
+
+
+# Delete a todo
+@app.delete("/todos/{todo_id}")
+async def delete_todo(todo_id: str):
+    deleted_todo = collection.delete_one({"_id": ObjectId(todo_id)})
+    if deleted_todo.deleted_count:
+        return {"message": "Todo deleted successfully"}
+    raise HTTPException(status_code=404, detail="Todo not found")
