b4da54e409c69d161375f35baefb3f419c5547f8
Jishnu-k-b <jishnu.kb17291@gmail.com>
2024-03-19 16:06:46 +0530

    Todo CRUD

--- app/apps.py
+++ app/apps.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pymongo import MongoClient
+from bson import ObjectId
 
+from .models import Todo
 
 app = FastAPI()
 
@@ -8,3 +10,14 @@ app = FastAPI()
 client = MongoClient("mongodb://localhost:27017/")
 db = client["todo_db"]
 collection = db["todo"]
+
+
+# Create todo
+@app.post("/todos/")
+async def create_todo(todo: Todo):
+    todo_dict = todo.model_dump()
+    inserted_id = collection.insert_one(todo_dict).inserted_id
+
+    todo_dict["_id"] = str(inserted_id)
+
+    return todo_dict
